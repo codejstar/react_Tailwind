71f0c8e3a55046fdb06eb6880387a08c7ba6ba73
code jstar <codejstar@gmail.com>
2025-12-15 16:39:27 +0530

    add RightText.jsx and ImageContainer.jsx in Page1Content.jsx

---
+++ src/component/Section1/Page1Content.jsx
@@ -0,0 +1,14 @@
+import ImageContainer from "./ImageContainer"
+import LeftText from "./LeftText"
+
+
+const Page1Content = () => {
+  return (
+    <div className="py-3  px-18 flex justify-between">
+    <LeftText />
+    <ImageContainer />
+    </div>
+  )
+}
+
+export default Page1Content
